412ad7424e477354ebcf74105ea71ae261c45b2e
许子衿 <1541213427@qq.com>
2026-01-22 15:43:20 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeLoginDTO.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 
 @Data
 @ApiModel(description = "员工登录时传递的数据模型")
-public class EmployeeLoginDTO implements Serializable {
+public class  EmployeeLoginDTO implements Serializable {
 
     @ApiModelProperty("用户名")
     private String username;
